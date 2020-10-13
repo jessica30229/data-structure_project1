527b10795affe3db9aa4e812142d3cbb27bfb30d
jessica30229 <jessica30229@gmail.com>
2020-10-13 18:03:42 +0800

    finish fstream

--- 107070004_proj1_first.cpp
+++ 107070004_proj1_first.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
-//#include <bits/stdc++.h>
+#include <fstream>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    for(int i = 1; i < argc; i++) {
-        printf("%s\n", argv[i]);
-    }
+int main(int argc, char *argv[]){
+  if(!argc) {
+    printf("usage: .exe filename\n");
+    exit(-1);
+  }
+  ifstream fin;
+  char line[128];
+  fin.open(argv[1], ios::in);
+  if(!fin){
+    cout<<"Fail to open file: "<<argv[1]<<endl;
+  }
+  cout<<"File Descriptor: "<<fin<<endl;
+  while(fin.getline(line, sizeof(line), '\n')){
+      cout<<line<<endl;
+  }
+  fin.close();
 
-    return 0;
+  ofstream fout;
+  fout.open("10707004_proj1.final", ios::out);
+  if(!fout){
+	  cout<<"Fail to open file: "<<"10707004_proj1.final"<<endl;
+  }
+  cout<<"File Descriptor: "<<fout<<endl;
+  fout<<"ans"<<endl; //寫入字串
+  fout.close();
+
+  return 0;
 }
 
 
