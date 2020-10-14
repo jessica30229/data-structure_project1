49a4a50e835d60d749380c8c152899094bb3771e
jessica30229 <jessica30229@gmail.com>
2020-10-14 21:54:14 +0800

    not just fall, should judge block sort first (wrong saved)

--- 107070004_proj1_first.cpp
+++ 107070004_proj1_first.cpp
@@ -59,27 +59,60 @@ void sort_O(){
   blockbroad[2][0] = 1; blockbroad[2][1] = 1; blockbroad[3][0] = 1; blockbroad[3][1] = 1;
 }
 
-void fall(int start_col, int move){
-  printf("%d %d\n", start_col, move);
+void deleterow(){
+
+}
+
+void update_gameboard(int stop_col, int stop_row){
+  printf("update_gameboard\n");
+  for(int i = stop_row; i > stop_row - 4; i--){
+    for(int j = stop_col; j < stop_col + 4; j++){
+      gamebroad[i][j] = blockbroad[i - stop_row + 3][j - stop_col];
+    }
+  }
+}
+
+bool fall(int start_col, int move){
+  int ok = 1;
+  for(int i = 0; i < command[0][0] - '0'; i++){ //8
+  //  printf("d1\n"); 
+   for(int j = start_col; j < start_col + 4; j++){
+    // printf("%d\n" , gamebroad[i][j]);
+    if(blockbroad[3][j - start_col] && gamebroad[i][j] && ok) {} // means hit.
+      return true;
+      // ok = 0;
+      // update_gameboard(start_col + move, i - 1);
+    else if(blockbroad[2][j - start_col] && gamebroad[i - 1][j] && ok){}
+
+    else if(blockbroad[1][j - start_col] && gamebroad[i - 2][j] && ok){}
+
+    else if(blockbroad[0][j - start_col] && gamebroad[i - 3][j] && ok){}
+
+    printf("d2\n");
+   }
+  }
+  printf("ok = %d\n", ok);
+  if(ok) update_gameboard(start_col + move, 0);
 }
 
 void play(char ch, int sort_num, int start_col, int move){
   if(ch == 'O'){
-      sort_O();
-    } else if(ch == 'T'){
-      sort_T(sort_num);
-    } else if(ch == 'L'){
-      sort_L(sort_num);
-    } else if(ch == 'J'){
-      sort_J(sort_num);
-    } else if(ch == 'S'){
-      sort_S(sort_num);
-    } else if(ch == 'Z'){
-      sort_Z(sort_num);
-    } else if(ch == 'I'){
-      sort_I(sort_num);
-    }
-    fall(start_col, move);
+    sort_O();
+  } else if(ch == 'T'){
+    sort_T(sort_num);
+  } else if(ch == 'L'){
+    sort_L(sort_num);
+  } else if(ch == 'J'){
+    sort_J(sort_num);
+  } else if(ch == 'S'){
+    sort_S(sort_num);
+  } else if(ch == 'Z'){
+    sort_Z(sort_num);
+  } else if(ch == 'I'){
+    sort_I(sort_num);
+  }
+  printf("play\n");
+  fall(start_col, move);
 }
 
 void start(){
@@ -97,15 +130,17 @@ void start(){
         blockbroad[i][j] = 0;
       }
     }
+    // the row and column all start from index 1 and all blocks shall never step out of boundary.
     if(ch == 'O'){
       int move = command[j][4] - '0';
       if(command[j][4] == '-')  move = - (command[j][5] - '0');
-      play(ch, 0, command[j][2] - '0', move);
+      play(ch, 0, command[j][2] - '0' - 1, move);
     } else {
       int move = command[j][5] - '0';
       if(command[j][5] == '-')  move = - (command[j][6] - '0');
-      play(ch, command[j][1] - '0', command[j][3] - '0', move);
+      play(ch, command[j][1] - '0', command[j][3] - '0' - 1, move);
     }
+    printf("start\n");
     j++;
   }
 }
@@ -133,7 +168,16 @@ int main (int argc, char *argv[]){
   }
   fin.close();
 
-  start();
+  if((command[0][0] - '0') > 15 || (command[0][2] - '0') > 40){
+    printf("invalid testcase.\n");
+  }
+  if(cnt - 1 > 1000){
+    printf("invalid testcase.\n");
+  }else{
+    // printf("ok\n");
+    start();
+  }
+
 
   //ouput the file
 
